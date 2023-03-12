b65899893da025cdbe4a9d908ad83aa6a33b428b
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:41:35 +0530

    Add files via upload

---
+++ Pattern_1/Pattern_1/Program.cs
@@ -0,0 +1,60 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+class program
+{
+    public static void waitFor1Seconds()
+    {
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        while (stopwatch.ElapsedMilliseconds < 1000)
+        {
+
+        }
+        stopwatch.Stop();
+
+    }
+
+    static void Main()
+    {
+        bool flag = false;
+        do
+        {
+
+
+            int values;
+            Console.WriteLine("Enter the size of pattern");
+            bool check = int.TryParse(Console.ReadLine(), out values);
+           
+            Console.ForegroundColor= ConsoleColor.Magenta;
+
+            if (check)
+            {
+                flag = true;
+          
+                for (int i = 0; i <= values; i++)
+                {
+                    for (int j = 0; j < i; j++)
+                    {
+                        Console.Write("♥");
+                        
+                    }
+                    Console.WriteLine();
+                    waitFor1Seconds();
+                    
+
+                }
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        } while (flag == false);
+
+        
+    }
+    
+
+}
+
