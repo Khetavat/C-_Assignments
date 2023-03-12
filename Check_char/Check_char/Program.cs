b28e3a1a20954361b641a69c8cb3ac49f8faa6ca
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:38:13 +0530

    Add files via upload

---
+++ Check_char/Check_char/Program.cs
@@ -0,0 +1,59 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    
+    static void Main()
+    {
+        bool flag = false;
+        do
+        {
+
+
+            Console.WriteLine("Enter a string");
+            string string1 = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Enter a character");
+
+                if (char.TryParse(Console.ReadLine(), out char chr))
+                {
+                    do
+                    {
+                        Console.WriteLine("Enter a index in range of 0 to {0}",string1.Length-1);
+                        if (int.TryParse(Console.ReadLine(), out int index))
+                        {
+                            flag = true;
+                            for (int i = index; i >= 0; i--)
+                            {
+                                if (string1[i] == chr)
+                                {
+                                    Console.WriteLine("String found at index {0}", index - i);
+                                    break;
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid input");
+                        }
+                    }while(!flag);
+
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input");
+                }
+            } while (!flag);
+            
+
+            Console.ReadLine();
+        } while (!flag);
+    }
+}
+
+
