fb0acc66d380ffb1b5761549299efd159a505a07
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:41:03 +0530

    Add files via upload

---
+++ Next_prime_number/Next_prime_number/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    static bool IsPrime(int number)
+    {
+        for(int i = 2; i < number/2; i++)
+        {
+            if(number%2 == 0)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+    static void Main()
+    {
+       
+        Console.WriteLine("Enter the number");
+        bool flag = true;
+        int value;
+        do
+        {
+            if (int.TryParse(Console.ReadLine(), out value))
+            {
+                flag = false;
+                value = value + 1;
+                while (IsPrime(value) == false)
+                {
+                    value= value + 1;
+                }
+                Console.WriteLine("The next prime number is {0}",value);
+
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+
+
+        } while (flag == true);
+
+        Console.ReadLine();
+
+    }
+}
+
