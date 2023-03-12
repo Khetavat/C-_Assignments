7d3375c2483b5d3f1c4e719696c73fb5968f67e0
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:36:48 +0530

    Add files via upload

---
+++ Alternate_Array/Alternate_Array/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+class program
+{
+    static void Main()
+    {
+        int values;
+        Console.WriteLine("How many values you want to add");
+        bool check = int.TryParse(Console.ReadLine(),out values);
+        int[] array = new int[values];
+        int i = 0;
+        while (i<values)
+        {
+            int number;
+            Console.WriteLine("Enter the {0} value", i + 1);
+            if (int.TryParse(Console.ReadLine(), out number))
+            {
+                array[i] = number;
+                i += 1;
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+        Console.WriteLine("The alternate array is");
+        for(int j = 0; j <values; j+=2) {
+            Console.WriteLine(array[j]);
+        }
+
+    }
+}
