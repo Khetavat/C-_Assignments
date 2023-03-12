23389df9e3fe80be30259e76215ccec1971a833d
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:38:06 +0530

    Add files via upload

---
+++ Boxing and UnBoxing/ConsoleApp5/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Collections;
+class Program
+{
+    static void Main()
+    {
+        //Boxing
+        int number = 23;
+        object Obj = 10;
+        object Obj2 = "vishal";
+
+        //Unboxing
+        int anothernumber = (int)Obj;
+        Console.WriteLine(anothernumber);
+
+        ArrayList arrayList = new ArrayList();
+        arrayList.Add(1);
+        arrayList.Add("data");
+       
+        Console.WriteLine("The sum of arrayList is {0}", (int)arrayList[0]+ (string)arrayList[1]);
+
+        Console.WriteLine(arrayList.Capacity);
+        arrayList.Add(1);
+        arrayList.Add(2);
+        arrayList.Add(1);
+        arrayList.Add(2);
+        Console.WriteLine(arrayList.Capacity);
+        Console.ReadLine();
+
+
+
+
+    }
+}
