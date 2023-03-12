6e16778fe979ac512a1d175b3e31d405fe422e1f
Khetavat <66075250+Khetavat@users.noreply.github.com>
2023-03-12 21:39:54 +0530

    Add files via upload

---
+++ Finally after return/ConsoleApp6/Finally.cs
@@ -0,0 +1,32 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class Finally
+{
+    Dictionary<int,string> keyValuePairs= new Dictionary<int,string>();
+
+    public Finally()
+    {
+        keyValuePairs[1] = "one";
+        keyValuePairs[2] = "two";
+           
+        
+    }
+    public string Show()
+    {
+        try
+        {
+            string Result = keyValuePairs[2];
+            Console.WriteLine(Result);
+            return Result;
+        }
+        catch {
+            Console.WriteLine("Error");
+            return "catch";
+        }
+        finally
+        {
+            Console.WriteLine("Finally Block");
+            
+        }
+    }
+}
---
+++ Finally after return/ConsoleApp6/Program.cs
@@ -0,0 +1,26 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+class Program
+{
+    
+   
+    static void Main()
+    {
+        int num = 10;
+        object obj = num;
+        set(ref obj);
+        Console.WriteLine(obj);
+        Console.WriteLine(num);
+
+        void set(ref object x)
+        {
+            x = 20;
+            
+        }
+
+
+        Finally finally1 = new Finally();
+        Console.WriteLine(finally1.Show());
+
+    }
+}
